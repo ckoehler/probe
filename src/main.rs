5aeff556e48ad3a811d3f433a4beecbd83c8d745
Christoph Koehler <christoph@zerodeviation.net>
2021-03-04 21:30:06 -0700

    Deserialize toml config into struct

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ mod util;
 use crate::probe::{ui, App};
 use crate::util::event::{Config, Event, Events};
 use argh::FromArgs;
+use serde::Deserialize;
+use std::fs;
 use std::{error::Error, io, time::Duration};
 use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
 use tui::{backend::TermionBackend, Terminal};
 
-/// Termion demo
+/// Probe Config
 #[derive(Debug, FromArgs)]
 struct Cli {
     /// time in ms between two ticks.
@@ -18,10 +20,27 @@ struct Cli {
     /// whether unicode symbols are used to improve the overall look of the app
     #[argh(option, default = "true")]
     enhanced_graphics: bool,
+    /// config file.
+    #[argh(option, default = "String::from(\"prober.toml\")")]
+    config: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Probes {
+    probes: Vec<Probe>,
+}
+#[derive(Debug, Deserialize)]
+struct Probe {
+    address: String,
+    mode: String,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cli: Cli = argh::from_env();
+    let config = fs::read_to_string(cli.config).expect("Something went wrong reading the file");
+
+    let deserialized: Probes = toml::from_str(&config).unwrap();
+    println!("{:?}", deserialized);
 
     let events = Events::with_config(Config {
         tick_rate: Duration::from_millis(cli.tick_rate),
@@ -34,7 +53,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    let mut app = App::new("Termion demo", cli.enhanced_graphics);
+    let mut app = App::new("Probe", cli.enhanced_graphics);
     loop {
         terminal.draw(|f| ui::draw(f, &mut app))?;
 
