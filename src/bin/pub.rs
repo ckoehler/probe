b733b6c3ad5aa644b4b1918a420bc6b0c9a7d496
Christoph Koehler <christoph@zerodeviation.net>
2025-01-27 18:42:26 -0700

    fix: deprecated functions in rand crate

--- src/bin/pub.rs
+++ src/bin/pub.rs
@@ -8,18 +8,18 @@ fn main() {
     socket.bind("tcp://127.0.0.1:5556").unwrap();
     socket.send("hello world!", 0).unwrap();
 
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rng();
     println!("Start server");
     println!("Start sending loop");
     loop {
-        let zipcode = rng.gen_range(10000..=10010);
-        let temperature = rng.gen_range(-80..=135);
-        let relhumidity = rng.gen_range(10..=60);
+        let zipcode = rng.random_range(10000..=10010);
+        let temperature = rng.random_range(-80..=135);
+        let relhumidity = rng.random_range(10..=60);
         let topic = "UNIT".to_string();
         let msg = format!("{} {} {}", zipcode, temperature, relhumidity);
         let msg = [topic, msg];
         socket.send_multipart(msg.iter(), 0).unwrap();
-        let delay = rng.gen_range(25..=2200);
+        let delay = rng.random_range(25..=2200);
         std::thread::sleep(Duration::from_millis(delay));
     }
 }
